28a1b75899d62d888cc9b178dfeb60be43e3f7c2
Михайлов Евгений <lewwadoo@gmail.com>
2019-12-05 19:38:05 +0300

    improved insert-todo and whether-add-org-capture-task; added whether-insert-org-capture-task and insert-tasks-by-autocondition

--- org-capture-complex.el
+++ org-capture-complex.el
@@ -1,14 +1,62 @@
+;; obsolete functions -->
+(defun insert-task-approve-email ()
+  "Approve email"
+  (interactive)
+  (kill-condition)
+  (setq org-inhibit-logging t)
+  (todo 2)
+  (insert "Подтвердить электронную почту")
+  (todo 3 1)
+  (insert "Зайти в аккаунт lewwadoo@gmail.com")
+  (todo 1)
+  (insert "Открыть непрочитанное письмо с сайта, требующего подтверждение почты")
+  (todo 1)
+  (insert "Перейти по ссылке из письма для подтверждения почты"))
+
+(defun privacy-policy-task ()
+  "Политика конфиденциальности приложения"
+  (move-beginning-of-line nil)
+;  (kill-line nil)
+  (kill-line nil)
+  (insert (concat (todo 3) "")))
+
+(defun insert-task-terms-of-service ()
+  "Insert tasks to deal with Terms of Service"
+  (interactive)
+  (kill-condition)
+  (setq org-inhibit-logging t)
+  (todo 3)
+  (insert "Просмотреть \"Условия использования приложения\"")
+  (todo 1)
+  (insert "Согласиться с \"Условиями использования приложения\""))
+
+(defun insert-task-term-of-blocking-account ()
+  "Insert task if term of blocking account exists"
+  (kill-condition)
+  (setq org-inhibit-logging t)
+  (todo 3)
+  (insert "Скопировать пункт соглашения, в котором описываются условия блокирования аккаунта")
+  (todo 1)
+  (insert "Выставить время истечения действия аккаунта на минимальный срок начала блокирования"))
+
+(defun human-prove-task-with-condition ()
+  "Add pass human prove task if needed"
+  (whether-add-next-task "Есть проверка на робота" "Пройти проверку на робота"))
+;; <-- obsolete functions
+
 (defun insert-todo (keyword &optional demote)
   "Insert todo"
   (with-temp-buffer
     (org-mode)
     (org-insert-todo-heading keyword)
     (org-todo keyword)
-    (if demote
-	(if (= 1 demote)
-	    (org-do-demote)
-	  (if (= -1 demote)
-	      (org-do-promote))))
+    (when demote
+      (while (> demote 0)
+	(org-do-demote)
+	(setq demote (1- demote)))
+      (while (< demote 0)
+	(org-do-promote)
+	(setq demote (1+ demote))))
     (buffer-substring-no-properties (point-min)(point-max))))
 
 (defun todo (keyword &optional demote)
@@ -43,9 +91,17 @@
 (defun whether-add-org-capture-task (question abbreviation &optional demote)
   "Whether add org-capture task"
   (if (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
-      (insert (concat "(org-capture 0 " "\"" abbreviation "\")"))
-    (kill-condition)
-    (kill-line nil)))
+      (insert (concat "(org-capture 0 " "\"" abbreviation "\")")))
+  (kill-condition)
+  (kill-line nil)))
+
+(defun whether-insert-org-capture-task (question true-abbreviation &optional false-abbreviation)
+  "Add TRUE-ABBREVIATION or FALSE-ABBREVIATION org-capture task"
+  (if (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
+      (insert (concat "(org-capture 0 " "\"" true-abbreviation "\")"))
+    (if false-abbreviation
+	(insert (concat "(org-capture 0 " "\"" false-abbreviation "\")"))))
+    (kill-condition))
 
 (defun whether-add-next-task (question task &optional demote)
   "Whether add next task"
@@ -57,6 +113,14 @@
 (defun whether-insert-tasks (question true-tasks &optional false-tasks)
   "Whether add tasks"
   (if (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
+      (insert-tasks true-tasks)
+    (if false-tasks
+	(insert-tasks false-tasks)
+      (kill-condition))))
+
+(defun insert-tasks-by-autocondition (condition true-tasks &optional false-tasks)
+  "Whether add tasks"
+  (if condition
       (insert-tasks true-tasks)
     (if false-tasks
 	(insert-tasks false-tasks)
