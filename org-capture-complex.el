2ec4856714111d61f7a9c3d04669a3bafa5037f5
Михайлов Евгений <lewwadoo@gmail.com>
2019-12-13 20:06:37 +0300

    a lot of org-capture templates have changed; moved obsolete functions to org-capture-templates

--- org-capture-complex.el
+++ org-capture-complex.el
@@ -39,10 +39,10 @@
   (todo 1)
   (insert "Выставить время истечения действия аккаунта на минимальный срок начала блокирования"))
 
-(defun human-prove-task-with-condition ()
-  "Add pass human prove task if needed"
-  (whether-add-next-task "Есть проверка на робота" "Пройти проверку на робота"))
 ;; <-- obsolete functions
+
+
+
 
 (defun insert-todo (keyword &optional demote)
   "Insert todo"
@@ -59,28 +59,30 @@
 	(setq demote (1+ demote))))
     (buffer-substring-no-properties (point-min)(point-max))))
 
-(defun todo (keyword &optional demote)
+(defun todo (keyword demote)
   "Return any org-todo heading"
-  (org-insert-heading-respect-content)
-      (org-todo keyword)
-  (if demote
-      (if (= 1 demote)
-	  (org-do-demote)
-	(if (= -1 demote)
-	    (org-do-promote)))))
-  
+  (org-insert-todo-heading keyword)
+;      (org-todo keyword)
+    (when demote
+      (while (> demote 0)
+	(org-do-demote)
+	(setq demote (1- demote)))
+      (while (< demote 0)
+	(org-do-promote)
+	(setq demote (1+ demote)))))
+
 (defun kill-condition ()
   "Kill condition for inserting task"
   (move-beginning-of-line nil)
   (kill-line nil))
 
-(defun insert-task (task keyword &optional demote)
+(defun insert-task (task keyword demote)
   "Insert task"
   (todo keyword demote)
   (insert task))
 
-(defun whether-insert-function (question true-function &optional false-function)
-  "Whether add next task"
+(defun whether-insert-function (question true-function false-function)
+  "Whether insert function"
   (if (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
       (call-interactively function)
     (if false-function
@@ -88,14 +90,14 @@
     (kill-condition)
     (kill-line nil))
 
-(defun whether-add-org-capture-task (question abbreviation &optional demote)
+(defun whether-add-org-capture-task (question abbreviation demote)
   "Whether add org-capture task"
-  (if (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
-      (insert (concat "(org-capture 0 " "\"" abbreviation "\")")))
+  (when (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
+    (insert (concat "(org-capture 0 " "\"" abbreviation "\")")))
   (kill-condition)
-  (kill-line nil)))
+  (kill-line nil))
 
-(defun whether-insert-org-capture-task (question true-abbreviation &optional false-abbreviation)
+(defun whether-insert-org-capture-task (question true-abbreviation false-abbreviation)
   "Add TRUE-ABBREVIATION or FALSE-ABBREVIATION org-capture task"
   (if (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
       (insert (concat "(org-capture 0 " "\"" true-abbreviation "\")"))
@@ -103,14 +105,15 @@
 	(insert (concat "(org-capture 0 " "\"" false-abbreviation "\")"))))
     (kill-condition))
 
-(defun whether-add-next-task (question task &optional demote)
+(defun whether-add-next-task (question task demote)
   "Whether add next task"
-  (if (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
-      (insert-task task 3 demote)
-    (kill-condition)
-    (kill-line nil)))
+  (kill-condition)
+  (kill-line nil)
+  (when (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
+    (insert-task task 3 demote)
+    (insert "\n")))
 
-(defun whether-insert-tasks (question true-tasks &optional false-tasks)
+(defun whether-insert-tasks (question true-tasks false-tasks)
   "Whether add tasks"
   (if (equal ?y (read-char-choice (concat question " (y/n)?") '(?y ?n)))
       (insert-tasks true-tasks)
@@ -118,7 +121,7 @@
 	(insert-tasks false-tasks)
       (kill-condition))))
 
-(defun insert-tasks-by-autocondition (condition true-tasks &optional false-tasks)
+(defun insert-tasks-by-autocondition (condition true-tasks false-tasks)
   "Whether add tasks"
   (if condition
       (insert-tasks true-tasks)
@@ -130,7 +133,12 @@
   "Insert TASKS"
   (kill-condition)
   (while tasks
-    (insert-task (car tasks) 1)
+    (insert-task (car tasks) 1 0)
     (setq tasks (cdr tasks))))
+
+(defun insert-org-capture-task (abbreviation)
+  "Insert org-capture task ABBREVIATION."
+  (kill-condition)
+  (org-capture 0 abbreviation))
   
 (provide 'org-capture-complex)
